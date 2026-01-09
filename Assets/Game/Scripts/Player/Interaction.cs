dfdb5644c1674fb8c7bfeb95dc83f25d706e3015
tercs <113709421+Terrces@users.noreply.github.com>
2026-01-09 18:06:10 +0500

    Small interaction refactoring

--- Assets/Game/Scripts/Player/Interaction.cs
+++ Assets/Game/Scripts/Player/Interaction.cs
@@ -33,12 +33,10 @@ public class Interaction : MonoBehaviour
     }
     private void TryInteract()
     {
-        Ray ray = new Ray(player.cameraTransform.position, player.cameraTransform.forward);
-        if (Physics.Raycast(ray, out RaycastHit hit, properties.GetInteractDistance(),interactionLayerMask))
-        {
-            if (hit.collider.TryGetComponent(out Iinteractable interactable)) interactable.Interact(this, GetArm(), properties.GetPickedUpMoveObjectSpeed(), properties.GetInteractDistance(), mask, properties);
-            if (hit.collider.TryGetComponent(out Object _object)) carriedObject = _object;
-            if (hit.collider.TryGetComponent(out Iusable usable)) carriedObjectUsableComponent = usable;
-        }
+        InteractWithObject interactWith = new InteractWithObject();
+        RaycastHit raycastHit = interactWith.GetRaycastHit(player.cameraTransform, properties, interactionLayerMask);
+        if (raycastHit.collider.TryGetComponent(out Iinteractable interactable)) interactable.Interact(this, GetArm(), properties.GetPickedUpMoveObjectSpeed(), properties.GetInteractDistance(), mask, properties);
+        if (raycastHit.collider.TryGetComponent(out Object _object)) carriedObject = _object;
+        if (raycastHit.collider.TryGetComponent(out Iusable usable)) carriedObjectUsableComponent = usable;
     }
 }
--- Assets/Game/Scripts/Utils/IntreractWithObject.cs
+++ Assets/Game/Scripts/Utils/IntreractWithObject.cs
@@ -8,4 +8,12 @@ public class InteractWithObject
         if (Physics.Raycast(ray, out RaycastHit hit,properties.GetInteractDistance(), 1, QueryTriggerInteraction.Ignore)) return hit;
         return new RaycastHit();
     }
+    
+    public RaycastHit GetRaycastHit(Transform point, Properties properties, LayerMask mask)
+    {
+        Ray ray = new Ray(point.position,point.forward);
+        if (Physics.Raycast(ray, out RaycastHit hit,properties.GetInteractDistance(), mask)) return hit;
+        return new RaycastHit();
+    }
+
 }
